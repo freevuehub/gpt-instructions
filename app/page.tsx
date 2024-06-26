980737a5ed0a65e7dd48529351c4d217b55dcab6
freevuehub <hackish113@gamil.com>
2024-06-27 00:33:21 +0900

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,22 @@
 'use server'
 
-const Home = () => {
-  return <main className="flex min-h-screen flex-col items-center justify-between p-24">Hello</main>
+import { headers } from 'next/headers'
+import { promises as fs } from 'fs'
+import { pipe, join } from '@fxts/core'
+
+const Home = async () => {
+  const header = headers()
+  const ip = (header.get('x-forwarded-for') ?? '127.0.0.1').split(',')[0]
+  const foo = await fs.readdir('/Users/freevue/Desktop/Project/frieren-ai')
+
+  // console.log(os)
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <p>{ip}</p>
+      {pipe(foo, join('/'))}
+    </main>
+  )
 }
 
 export default Home
